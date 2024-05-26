400a2b55c49138d5a516c43a93a805c36c3fee69
johannestigges <johannes.tigges@freenet.de>
2024-05-26 14:21:52 +0200

    update to angular 18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   { path: 'participate', component: ParticipateComponent },
   { path: 'protocol', component: ProtocolComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/participants', pathMatch: 'full' },
   { path: '**', redirectTo: '/login', pathMatch:'full' },
 ];
 
